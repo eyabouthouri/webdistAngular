3bcc5a85cc4c714f157e88e4ccff3fb50398c476
DorsafCharfeddine <dorsaf.charfeddine@esprit.tn>
2023-10-22 19:31:58 +0100

    virgule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProjetComponent } from './frontoffice/projet/projet.component';
 
 
 const routes: Routes = [
