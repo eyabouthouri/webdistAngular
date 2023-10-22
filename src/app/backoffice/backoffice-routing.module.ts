26c83b47714b8ff2235d43c3ce4b8c6c67837678
eyabouthouri <eya.bouthouri@esprit.tn>
2023-10-22 20:21:17 +0200

    eya

--- src/app/backoffice/backoffice-routing.module.ts
+++ src/app/backoffice/backoffice-routing.module.ts
@@ -4,9 +4,12 @@ import { BackofficeComponent } from './backoffice.component';
 import { FormationAdminComponent } from './formation-admin/formation-admin.component';
 import { EventsAdminComponent } from './events-admin/events-admin.component';
 import { EventsAddComponent } from './events-add/events-add.component';
+import { ProjetbackComponent } from './projetback/projetback.component';
+import { CondidaturebackComponent } from './condidatureback/condidatureback.component';
 
 const routes: Routes = [{ path: '', component: BackofficeComponent },{path:'back',component:FormationAdminComponent},
-
+{ path: 'projetback', component:ProjetbackComponent },
+{ path: 'condidtback/:id', component:CondidaturebackComponent },
 {path:'adminevent',component:EventsAdminComponent},
 {path:'addevent',component:EventsAddComponent}
 
